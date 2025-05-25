59efecdb6cb6ae33c13fc3dd8ebe920c14ebbbe3
masahirog <masahiro11g@gmail.com>
2025-05-25 16:57:07 +0900

    no message

---
+++ routes/training.py
@@ -0,0 +1,10 @@
+# routes/training.py
+
+from flask import Blueprint, render_template
+
+training_bp = Blueprint('training', __name__, url_prefix='/training')
+
+@training_bp.route('/')
+def training_page():
+    """学習実行ページ"""
+    return render_template('training_execution.html')
